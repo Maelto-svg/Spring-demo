62a80fef6a43a94d73e2bdaff0e6b817713eb126
Maelwen THOMAS <thomas.maelwen@gmail.com>
2024-09-20 15:29:21 +0200

    Second Commit

---
+++ src/main/java/com/emse/spring/automacorp/hello/DummyUserService.java
@@ -0,0 +1,22 @@
+package com.emse.spring.automacorp.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DummyUserService implements UserService{
+    private final ConsoleGreetingService consoleGreetingService;
+
+    public DummyUserService(ConsoleGreetingService consoleGreetingService){
+        this.consoleGreetingService=consoleGreetingService;
+    }
+
+    @Override
+    public void greetAll(List<String> name) {
+        for(String s : name){
+            this.consoleGreetingService.greet(s);
+        }
+    }
+}
